ad8ff5f661a46b39ef37bd88f1b16f5238ac681d
Tim Levett <levett@wisc.edu>
2015-10-19 15:42:26 -0500

    move stuff to static site

--- src/main/webapp/config.js
+++ src/main/webapp/config.js
@@ -10,18 +10,18 @@ define([], function() {
     ],
 
     paths: {
-        'angular'       : "bower_components/angular/angular.min",
-        'angular-mocks' : "bower_components/angular-mocks/angular-mocks",
+        'angular'       : "./bower_components/angular/angular.min",
+        'angular-mocks' : "./bower_components/angular-mocks/angular-mocks",
         'app-config'    : "js/app-config",
         'frame-config'  : "js/frame-config",
-        'jquery'        : "bower_components/jquery/dist/jquery.min",
-        'jquery-ui'     : "bower_components/jquery-ui/jquery-ui.min",
-        'ngRoute'       : "bower_components/angular-route/angular-route.min",
-        'ngSanitize'    : "bower_components/angular-sanitize/angular-sanitize.min",
-        'ngStorage'     : "bower_components/ngstorage/ngStorage.min",
+        'jquery'        : "./bower_components/jquery/dist/jquery.min",
+        'jquery-ui'     : "./bower_components/jquery-ui/jquery-ui.min",
+        'ngRoute'       : "./bower_components/angular-route/angular-route.min",
+        'ngSanitize'    : "./bower_components/angular-sanitize/angular-sanitize.min",
+        'ngStorage'     : "./bower_components/ngstorage/ngStorage.min",
         'sortable'      : "js/sortable",
-        'ui-bootstrap'  : "bower_components/angular-bootstrap/ui-bootstrap-tpls.min",
-        'ui-gravatar'   : "bower_components/angular-gravatar/build/angular-gravatar.min"
+        'ui-bootstrap'  : "./bower_components/angular-bootstrap/ui-bootstrap-tpls.min",
+        'ui-gravatar'   : "./bower_components/angular-gravatar/build/angular-gravatar.min"
         // Use ui-bootstrap instead of bootstrap or uw-ui-toolkit.  See https://angular-ui.github.io/bootstrap/
         //'uw-ui-toolkit' : "bower_components/uw-ui-toolkit/dist/js/uw-ui-toolkit.min"
     },
